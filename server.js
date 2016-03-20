e9910548aab5041b34ede7786aa8012dbb4a6001
Andrew P. Wilson <luthien256@gmail.com>
2016-03-19 20:20:57 -0700

    Add https support.

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+
 var express = require('express');
 var app = express();
 var bodyParser  = require('body-parser');
@@ -14,3 +22,9 @@ app.get('/', function(req, res) {
 var server = app.listen(process.env.PORT || 3000, function() {
   console.log('Express Server listening on port %s', server.address().port);
 });
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
