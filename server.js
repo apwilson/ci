6235bc6684b8729cd9d0f12607e75ea74e349f2e
Andrew P. Wilson <luthien256@gmail.com>
2016-03-19 21:18:07 -0700

    Fix running script.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-  console.log('request %s from %s with %s', req.baseUrl, req.ip, JSON.stringify(req.body, null, 2));
+  console.log('request %s from %s with %s %s', req.baseUrl, req.ip, JSON.stringify(req.body, null, 2), process.cwd());
   res.send('Hello World!');
 });
 
@@ -23,21 +23,25 @@ app.post('/receivepost', function(req, res) {
   console.log('request %s from %s with %s', req.baseUrl, req.ip, JSON.stringify(req.body, null, 2));
   const id = req.body['head_commit']['id'];
   const repo = req.body['repository']['html_url'];
-  const clone = spawn('clone.sh', [id, repo]);
+  const clone = spawn('sh', ['clone.sh', id, repo], {
+  cwd: process.cwd(),
+  env: process.env
+});
   clone.stdout.on('data', function(data) {
-    console.log('stdout: ${data}');
+    console.log('stdout: %s', data);
   });
 
   clone.stderr.on('data', function(data) {
-    console.log('stderr: ${data}');
+    console.log('stderr: %s', data);
   });
 
   clone.on('close', function(code) {
-    console.log('child process exited with code ${code}');
-  });});
+    console.log('child process exited with code %s',code);
+  });
+});
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(80);
+httpServer.listen(8080);
 httpsServer.listen(443);
